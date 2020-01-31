d12a6f6af2064c5c2023a396a95a089521d51849
AndrewA0112 <andrew.mkal@outlook.com>
2020-01-31 11:54:14 -0600

    sprint mvp done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,30 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+ADD = 0b10100000
+POP = 0b01000110
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+PUSH = 0b01000101
+CALL = 0b01010000
+RET = 0b00010001
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * (2**8)
+        self.ram = [0] * 256
         self.pc = 0
         self.register = [0] * 8
         self.register[7] = 0xFF
+        self.fl = [0] * 8
 
     def load(self, file_name):
         """Load a program into memory."""
@@ -31,7 +46,19 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+
+        elif op == "CMP":
+            self.fl[5] = 0
+            self.fl[6] = 0
+            self.fl[7] = 0
+
+            if self.register[reg_a] == self.register[reg_b]:
+                self.fl[7] = 1
+            elif self.register[reg_a] > self.register[reg_b]:
+                self.fl[6] = 1
+            elif self.register[reg_a] < self.register[reg_b]:
+                self.fl[5] = 1
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -63,35 +90,70 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if ir == 0b00000001: # Halt
+            if ir == HLT:
                 running = False
                 self.pc += 1
 
-            elif ir == 0b01000111: # Print
+            elif ir == PRN:
                 print(self.register[operand_a])
-                self.pc += 2
+                self.pc += 2  
 
-            elif ir == 0b10000010: # Set register value
+            elif ir == LDI:
                 self.register[operand_a] = operand_b
                 self.pc += 3
 
-            elif ir == 0b10100010: # Multiply registers
+            elif ir == MUL:
                 result = self.register[operand_a] * self.register[operand_b]
                 self.register[operand_a] = result
                 self.pc += 3
+            
+            elif ir == ADD:
+                result = self.register[operand_a] + self.register[operand_b]
+                self.register[operand_a] = result
+                self.pc += 3
 
-            elif ir == 0b01000101: # Push register value to stack
+            elif ir == PUSH:
                 self.register[7] -= 1
                 self.ram[self.register[7]] = self.register[operand_a]
                 self.pc += 2
 
-            elif ir == 0b01000110: # Pop register value from stack
+            elif ir == POP:
                 self.register[operand_a] = self.ram[self.register[7]]
-                self.register[7] -= 1
+                self.register[7] += 1
                 self.pc += 2
 
+            elif ir == CALL:
+                ret_address = self.pc + 2
+                self.register[7] -= 1
+                self.ram[self.register[7]] = ret_address
+
+                self.pc = self.register[operand_a]
+            
+            elif ir == RET:
+                self.pc = self.ram[self.register[7]]
+                self.register[7] += 1
+            
+            elif ir == CMP:
+                self.alu('CMP', operand_a, operand_b)
+                self.pc += 3
+            
+            elif ir == JMP:
+                self.pc = self.register[operand_a]
+            
+            elif ir == JEQ:
+                if self.fl[7] == 1:
+                    self.pc = self.register[operand_a]
+                else:
+                    self.pc += 2
+
+            elif ir == JNE:
+                if self.fl[7] == 0:
+                    self.pc = self.register[operand_a]
+                else:
+                    self.pc += 2
+
             else:
-                print(f'Unknown instruction: {ir}')
+                print(f'Unknown instruction: {ir} {self.pc}')
                 sys.exit(1)
 
     def ram_read(self, address):
